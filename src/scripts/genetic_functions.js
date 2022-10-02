a927bc36d1b0874c30be65663febe0b130fe61f8
HobbesWSL <jessewilson@outlook.com>
2022-10-03 12:48:00 +1300

    add support for zero and negative fitness

--- src/scripts/genetic_functions.js
+++ src/scripts/genetic_functions.js
@@ -1,4 +1,11 @@
 /**
+ * Questions:
+ * 1. What do we wanted to do with negative and zero fitness values?
+ *    - they have zero probability of being selected? OR
+ *    - do we want to give them a small probability of being selected? (relative to the rest)
+ */
+
+/**
  * Creates an initial population of n random individuals
  * @param {number} n - number of individuals in initial population
  * @param {function} creation_function - function to create an individual
@@ -27,9 +34,10 @@ export function evaluateFitness(population, fitness_function, target_value) {
  * @param {Array<Object>} population - array of individuals
  * @param {number} n - number of individuals in new population
  * @param {function} crossover_function - a function to create a new individual from two parents
+ * @param {bool} ascending - whether to maximize or minimize the fitness function
  * @returns {Array<Object>} - the next generation of individuals
  */
-export function crossover(population, n, crossover_function) {
+export function crossover(population, n, crossover_function, ascending) {
   // raises an error if the population size is less than 2
   if (population.length < 2) {
     throw new Error("population size must be greater than 1");
@@ -37,7 +45,11 @@ export function crossover(population, n, crossover_function) {
   const new_population = [];
   for (let i = 0; i < n; i++) {
     // it might be interesting to implement monogamy, polygamy, multiple children, etc.
-    const [parent1, parent2] = weightedSampleWithoutReplacement(population, 2);
+    const [parent1, parent2] = weightedSampleWithoutReplacement(
+      population,
+      2,
+      ascending
+    );
     new_population.push(crossover_function(parent1, parent2));
   }
   return new_population;
@@ -57,13 +69,16 @@ export function mutate(population, mutation_function, mutation_rate) {
 }
 
 /**
- * Returns the individual with the highest fitness in a population
- * @param {Array<Object>} population -
+ * Returns the fittest individual in a population
+ * @param {Array<Object>} population - array of individuals
+ * @param {bool} ascending - whether to maximize or minimize the fitness function
  * @returns {Object} - individual with highest fitness
  */
-export function fittestIndividual(population) {
+export function fittestIndividual(population, ascending) {
   const fitness = population.map((individual) => individual.fitness);
-  const index = fitness.indexOf(Math.max(...fitness));
+  const index = ascending
+    ? fitness.indexOf(Math.max(...fitness))
+    : fitness.indexOf(Math.min(...fitness));
   return population[index];
 }
 
@@ -71,29 +86,45 @@ export function fittestIndividual(population) {
  * Return a random sample without replacement weighted by fitness
  * @param {Array<Object>} population - array of individuals
  * @param {number} n - number of individuals to sample
+ * @param {bool} ascending - whether to maximize or minimize the fitness function
  * @returns {Array<Object>} - array of sampled individuals
  */
-export function weightedSampleWithoutReplacement(population, n) {
+export function weightedSampleWithoutReplacement(population, n, ascending) {
   // raises an error if n is greater than the population size
   if (n > population.length) {
     throw new Error("n must be less than or equal to the population size");
   }
   // get weights based on relative fitness of individuals
-  const weights = relativeCumulativeFitness(population);
+  const weights = relativeCumulativeFitness(population, ascending);
   // get n unique indices
   const indices = randomUniqueIndices(weights, n);
   // return the individuals at the indices
-  // subtract 1 from the indices because the weights array has an extra element at the beginning
-  return indices.map((index) => population[index - 1]);
+  return indices.map((index) => population[index]);
 }
 
 /**
  * Return an array of relative cumulative fitness of a population
  * @param {Array<Object>} population
+ * @param {bool} ascending - whether to maximize or minimize the fitness function
  * @returns {Array<number>} - array of relative cumulative fitness
  */
-export function relativeCumulativeFitness(population) {
-  const fitness = population.map((individual) => individual.fitness);
+export function relativeCumulativeFitness(population, ascending) {
+  // get the fitness values for all individuals
+  let fitness = population.map((individual) => individual.fitness);
+  // if any value is negative, add absolute value of min to all values to make all positive
+  if (fitness.some((value) => value < 0)) {
+    const min = Math.min(...fitness);
+    fitness = fitness.map((value) => value + Math.abs(min));
+  }
+  // if any values are 0, add 1 to all values to avoid division by 0
+  if (fitness.some((value) => value === 0)) {
+    fitness = fitness.map((value) => value + 1);
+  }
+  // if ascending is false, use 1 / fitness to invert the values
+  if (!ascending) {
+    fitness = fitness.map((value) => 1 / value);
+  }
+  // normalize the values
   const total_fitness = fitness.reduce(
     (total, individual) => total + individual,
     0
@@ -101,10 +132,13 @@ export function relativeCumulativeFitness(population) {
   const relative_fitness = fitness.map(
     (individual) => individual / total_fitness
   );
-  // return the cumulative sum of the relative fitness
+  // return the cumulative sum of the relative fitness values
   return relative_fitness.reduce(
-    (store, current, index) => [...store, store[index] + current],
-    [0]
+    (cumulative, individual, index) => [
+      ...cumulative,
+      individual + (cumulative[index - 1] || 0),
+    ],
+    []
   );
 }
 
--- src/scripts/genetic_functions.test.js
+++ src/scripts/genetic_functions.test.js
@@ -1,19 +1,19 @@
-import * as genetic from "./genetic_functions";
+import * as genetic_fn from "./genetic_functions";
 import { mockRandom, resetMockRandom } from "jest-mock-random";
 
 describe("randomPopulation", () => {
   test("returns an array of length n", () => {
-    const population = genetic.randomPopulation(100, () => 1);
+    const population = genetic_fn.randomPopulation(100, () => 1);
     expect(population).toBeInstanceOf(Array);
     expect(population.length).toBe(100);
   });
   test("calls the creation function n times", () => {
     const creation_function = jest.fn();
-    genetic.randomPopulation(100, creation_function);
+    genetic_fn.randomPopulation(100, creation_function);
     expect(creation_function).toHaveBeenCalledTimes(100);
   });
   test("returns an array of the values returned by the creation function", () => {
-    const population = genetic.randomPopulation(100, () => 1);
+    const population = genetic_fn.randomPopulation(100, () => 1);
     expect(population.every((individual) => individual === 1)).toBe(true);
   });
 });
@@ -22,7 +22,7 @@ describe("evaluateFitness", () => {
   test("returns an array of individuals with fitness values", () => {
     const population = [{ value: 1 }, { value: 2 }, { value: 3 }];
     const fitness_function = (individual) => individual.value;
-    const evaluated_population = genetic.evaluateFitness(
+    const evaluated_population = genetic_fn.evaluateFitness(
       population,
       fitness_function
     );
@@ -35,7 +35,7 @@ describe("evaluateFitness", () => {
   test("calls the fitness function on each individual", () => {
     const population = [{ value: 1 }, { value: 2 }, { value: 3 }];
     const fitness_function = jest.fn();
-    genetic.evaluateFitness(population, fitness_function);
+    genetic_fn.evaluateFitness(population, fitness_function);
     expect(fitness_function).toHaveBeenCalledTimes(3);
     expect(fitness_function).toHaveBeenCalledWith({ value: 1 }, undefined);
     expect(fitness_function).toHaveBeenCalledWith({ value: 3 }, undefined);
@@ -43,7 +43,7 @@ describe("evaluateFitness", () => {
   test("uses the target value if provided", () => {
     const population = [{ value: 1 }, { value: 2 }, { value: 3 }];
     const fitness_function = (individual, target) => individual.value + target;
-    const evaluated_population = genetic.evaluateFitness(
+    const evaluated_population = genetic_fn.evaluateFitness(
       population,
       fitness_function,
       10
@@ -57,7 +57,7 @@ describe("evaluateFitness", () => {
 });
 
 describe("fittestIndividual", () => {
-  test("returns the individual with the highest fitness", () => {
+  test("returns the individual with the highest fitness when ascending is true", () => {
     const population = [
       { value: 1, fitness: 1 },
       { value: 2, fitness: 2 },
@@ -65,8 +65,19 @@ describe("fittestIndividual", () => {
       { value: 4, fitness: 4 },
       { value: 5, fitness: 5 },
     ];
-    const best_individual = genetic.fittestIndividual(population);
+    const best_individual = genetic_fn.fittestIndividual(population, true);
     expect(best_individual).toEqual({ value: 5, fitness: 5 });
+  });
+  test("returns the individual with the lowest fitness when ascending is false", () => {
+    const population = [
+      { value: 1, fitness: 1 },
+      { value: 2, fitness: 2 },
+      { value: 3, fitness: 3 },
+      { value: 4, fitness: 4 },
+      { value: 5, fitness: 5 },
+    ];
+    const best_individual = genetic_fn.fittestIndividual(population, false);
+    expect(best_individual).toEqual({ value: 1, fitness: 1 });
   });
 });
 
@@ -80,7 +91,7 @@ describe("mutate", () => {
       { value: 5, fitness: 5 },
     ];
     const mutation_function = () => 1;
-    const mutated_population = genetic.mutate(
+    const mutated_population = genetic_fn.mutate(
       population,
       mutation_function,
       0.01
@@ -98,7 +109,7 @@ describe("mutate", () => {
     ];
     const mutation_function = (individual, mutation_rate) =>
       individual.value * mutation_rate;
-    const mutated_population = genetic.mutate(
+    const mutated_population = genetic_fn.mutate(
       population,
       mutation_function,
       10
@@ -116,20 +127,74 @@ describe("relativeCumulativeFitness", () => {
       { value: 4, fitness: 4 },
       { value: 5, fitness: 5 },
     ];
-    const relative_fitness = genetic.relativeCumulativeFitness(population);
+    const relative_fitness = genetic_fn.relativeCumulativeFitness(
+      population,
+      true
+    );
     expect(relative_fitness).toBeInstanceOf(Array);
-    // + 1 because the first element is 0
-    expect(relative_fitness.length).toEqual(population.length + 1);
+    expect(relative_fitness.length).toEqual(population.length);
   });
-  test("returned array has expected values", () => {
+  test("adds abs of the min value + 1 when there are negative values", () => {
+    const population = [
+      { value: 1, fitness: 1 }, // 0.3636
+      { value: 2, fitness: -2 }, // 0.4545
+      { value: 3, fitness: 3 }, // 1
+    ];
+    const relative_fitness = genetic_fn.relativeCumulativeFitness(
+      population,
+      true
+    );
+    const expected_values = [0.3636, 0.4545, 1];
+    relative_fitness.forEach((value, index) => {
+      // handle floating point precision errors
+      expect(value).toBeCloseTo(expected_values[index]);
+    });
+  });
+  test("returns an array of equal probabilities when all fitness values are zero", () => {
+    const population = [
+      { value: 1, fitness: 0 },
+      { value: 2, fitness: 0 },
+      { value: 3, fitness: 0 },
+    ];
+    const relative_fitness = genetic_fn.relativeCumulativeFitness(
+      population,
+      true
+    );
+    const expected_values = [0.3333, 0.6666, 1];
+    relative_fitness.forEach((value, index) => {
+      // handle floating point precision errors
+      expect(value).toBeCloseTo(expected_values[index]);
+    });
+  });
+  test("returned array has expected values when ascending is true", () => {
     const population = [
       { value: 1, fitness: 1 },
       { value: 2, fitness: 2 },
       { value: 3, fitness: 3 },
       { value: 4, fitness: 4 },
     ];
-    const relative_fitness = genetic.relativeCumulativeFitness(population);
-    const expected_values = [0, 0.1, 0.3, 0.6, 1];
+    const relative_fitness = genetic_fn.relativeCumulativeFitness(
+      population,
+      true
+    );
+    const expected_values = [0.1, 0.3, 0.6, 1];
+    relative_fitness.forEach((value, index) => {
+      // handle floating point precision errors
+      expect(value).toBeCloseTo(expected_values[index]);
+    });
+  });
+  test("returned array has expected values when ascending is false", () => {
+    const population = [
+      { value: 1, fitness: 1 },
+      { value: 2, fitness: 2 },
+      { value: 3, fitness: 3 },
+      { value: 4, fitness: 4 },
+    ];
+    const relative_fitness = genetic_fn.relativeCumulativeFitness(
+      population,
+      false
+    );
+    const expected_values = [0.48, 0.72, 0.88, 1];
     relative_fitness.forEach((value, index) => {
       // handle floating point precision errors
       expect(value).toBeCloseTo(expected_values[index]);
@@ -140,21 +205,21 @@ describe("relativeCumulativeFitness", () => {
 describe("randomUniqueIndices", () => {
   test("returns an array of length n", () => {
     const weights = [0, 0.1, 0.3, 0.6, 1];
-    const indices = genetic.randomUniqueIndices(weights, 3);
+    const indices = genetic_fn.randomUniqueIndices(weights, 3);
     expect(indices).toBeInstanceOf(Array);
     expect(indices.length).toBe(3);
   });
   test("returned indices are unique", () => {
     const weights = [0, 0.1, 0.3, 0.6, 1];
     mockRandom([0.1, 0.1, 0.38, 0.95]);
-    const indices = genetic.randomUniqueIndices(weights, 3);
+    const indices = genetic_fn.randomUniqueIndices(weights, 3);
     const unique_indices = new Set(indices);
     expect(unique_indices.size).toBe(indices.length);
     resetMockRandom();
   });
   test("returned indices are within the range of the weights array", () => {
     const weights = [0, 0.1, 0.3, 0.6, 1];
-    const indices = genetic.randomUniqueIndices(weights, 3);
+    const indices = genetic_fn.randomUniqueIndices(weights, 3);
     indices.forEach((index) => {
       expect(index).toBeGreaterThanOrEqual(0);
       expect(index).toBeLessThan(weights.length);
@@ -164,7 +229,7 @@ describe("randomUniqueIndices", () => {
   test("the random number is mapped to the correct index", () => {
     const weights = [0, 0.1, 0.3, 0.6, 1];
     mockRandom([0.05, 0.1, 0.38, 0.95]);
-    const indices = genetic.randomUniqueIndices(weights, 4);
+    const indices = genetic_fn.randomUniqueIndices(weights, 4);
     expect(indices).toEqual([1, 2, 3, 4]);
     resetMockRandom();
   });
@@ -177,7 +242,7 @@ describe("weightedSampleWithoutReplacement", () => {
       { value: 2, fitness: 2 },
     ];
     expect(() =>
-      genetic.weightedSampleWithoutReplacement(population, 3)
+      genetic_fn.weightedSampleWithoutReplacement(population, 3, true)
     ).toThrowError("n must be less than or equal to the population size");
   });
   test("returns an array of length n", () => {
@@ -185,11 +250,15 @@ describe("weightedSampleWithoutReplacement", () => {
       { value: 1, fitness: 1 },
       { value: 2, fitness: 2 },
     ];
-    const sample = genetic.weightedSampleWithoutReplacement(population, 1);
+    const sample = genetic_fn.weightedSampleWithoutReplacement(
+      population,
+      1,
+      true
+    );
     expect(sample).toBeInstanceOf(Array);
     expect(sample.length).toBe(1);
   });
-  test("returns the correct sample", () => {
+  test("returns the correct sample when ascending is true", () => {
     const population = [
       { value: 1, fitness: 1 }, // 0.1
       { value: 2, fitness: 2 }, // 0.3
@@ -197,8 +266,28 @@ describe("weightedSampleWithoutReplacement", () => {
       { value: 4, fitness: 4 }, // 1
     ];
     mockRandom([0.01, 0.05, 0.38]);
-    const sample = genetic.weightedSampleWithoutReplacement(population, 2);
+    const sample = genetic_fn.weightedSampleWithoutReplacement(
+      population,
+      2,
+      true
+    );
     expect(sample).toEqual([population[0], population[2]]);
+    resetMockRandom();
+  });
+  test("returns the correct sample when ascending is false", () => {
+    const population = [
+      { value: 1, fitness: 1 }, // 0.48
+      { value: 2, fitness: 2 }, // 0.72
+      { value: 3, fitness: 3 }, // 0.88
+      { value: 4, fitness: 4 }, // 1
+    ];
+    mockRandom([0.01, 0.5, 0.92]);
+    const sample = genetic_fn.weightedSampleWithoutReplacement(
+      population,
+      3,
+      false
+    );
+    expect(sample).toEqual([population[0], population[1], population[3]]);
     resetMockRandom();
   });
 });
@@ -208,7 +297,7 @@ describe("crossover", () => {
     const population = [{ value: 1, fitness: 1 }];
     const crossover_function = jest.fn();
     expect(() =>
-      genetic.crossover(population, 1, crossover_function)
+      genetic_fn.crossover(population, 1, crossover_function, true)
     ).toThrowError("population size must be greater than 1");
   });
   test("returns an array of length n", () => {
@@ -220,7 +309,12 @@ describe("crossover", () => {
       { value: 5, fitness: 5 },
     ];
     const crossover_function = () => 1;
-    const children = genetic.crossover(population, 10, crossover_function);
+    const children = genetic_fn.crossover(
+      population,
+      10,
+      crossover_function,
+      true
+    );
     expect(children).toBeInstanceOf(Array);
     expect(children.length).toBe(10);
   });
@@ -235,7 +329,12 @@ describe("crossover", () => {
     const crossover_function = (parent_1, parent_2) => ({
       value: parent_1.value + parent_2.value,
     });
-    const children = genetic.crossover(population, 2, crossover_function);
+    const children = genetic_fn.crossover(
+      population,
+      2,
+      crossover_function,
+      true
+    );
     expect(children).toEqual([{ value: 3 }, { value: 7 }]);
     resetMockRandom();
   });
